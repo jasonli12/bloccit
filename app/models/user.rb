3c4418c66410f96618dfe65adfa4573e977ed04c
Jason Li <jason.li0102@gmail.com>
2017-05-11 22:39:30 -0700

    Assignment 24 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
-
+  before_save :capitalize_name
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -12,4 +12,15 @@ class User < ApplicationRecord
               uniqueness: { case_sensitive: false },
               length: { minimum: 3, maximum: 254 }
     has_secure_password
+
+    def capitalize_name
+      if name
+        name_array = []
+        name.split.each do |x|
+          name_array.push(x.capitalize)
+        end
+
+        self.name = name_array.join(" ")
+      end
+    end
 end
